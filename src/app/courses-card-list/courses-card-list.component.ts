c9641e9780f720a0cbca91c283031721bf6b67e9
syamanashi <sutton@suttonsoft.com>
2025-07-01 11:37:14 -0400

    CRUD Update - Opening the Edit Course Dialog

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, input, output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Course } from '../models/course.model';
 import { MatDialog } from '@angular/material/dialog';
+import { openEditCourseDialog } from '../edit-course-dialog/edit-course-dialog.component';
 
 @Component({
   selector: 'courses-card-list',
@@ -11,4 +12,15 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class CoursesCardListComponent {
   courses = input.required<Course[]>();
+  dialog = inject(MatDialog);
+
+  async onEditCourse(course: Course) {
+    const newCourse = await openEditCourseDialog(this.dialog, {
+      mode: 'update',
+      title: 'Update Existing Course',
+      course,
+    });
+
+    console.log('Course edited:', newCourse);
+  }
 }
