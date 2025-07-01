53f60e6639d61ab9a160f574870e6c29429216d7
syamanashi <sutton@suttonsoft.com>
2025-07-01 15:03:19 -0400

    Skipping The Loading Indicator Using an HTTP Context

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,10 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpContext } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { firstValueFrom } from 'rxjs';
 import { Course } from '../models/course.model';
 import { GetCoursesResponse } from '../models/get-courses.response';
+import { SkipLoading } from '../loading/skip-loading.component';
 
 @Injectable({
   providedIn: 'root',
@@ -14,7 +15,10 @@ export class CoursesService {
 
   async loadAllCourses(): Promise<Course[]> {
     const courses$ = this.http.get<GetCoursesResponse>(
-      `${this.env.apiRoot}/courses`
+      `${this.env.apiRoot}/courses`,
+      {
+        context: new HttpContext().set(SkipLoading, true),
+      }
     );
     const response = await firstValueFrom(courses$);
     return response.courses;
--- src/app/services/loading.interceptor.ts
+++ src/app/services/loading.interceptor.ts
@@ -6,11 +6,17 @@ import {
 import { inject } from '@angular/core';
 import { LoadingService } from '../loading/loading.service';
 import { finalize } from 'rxjs';
+import { SkipLoading } from '../loading/skip-loading.component';
 
 export const loadingInterceptor: HttpInterceptorFn = (
   req: HttpRequest<unknown>,
   next: HttpHandlerFn
 ) => {
+  // If the SkipLoading HttpContextToken is on, then do nothing and bypass all loadingOn logic.
+  if (req.context.get(SkipLoading)) {
+    return next(req);
+  }
+
   const loadingService = inject(LoadingService);
   loadingService.loadingOn();
   return next(req).pipe(
