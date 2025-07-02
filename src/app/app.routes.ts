e8425766e23a3aff36b954e20bf95aa2256efb83
syamanashi <sutton@suttonsoft.com>
2025-07-01 21:31:41 -0400

    Implementing an Authentication Guard With Signals

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,33 +1,35 @@
-import {Routes} from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {LoginComponent} from "./login/login.component";
-import {LessonsComponent} from "./lessons/lessons.component";
-import {ResourceDemoComponent} from "./resource-demo/resource-demo.component";
-import {LinkedSignalDemoComponent} from "./linked-signal/linked-signal-demo.component";
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LessonsComponent } from './lessons/lessons.component';
+import { ResourceDemoComponent } from './resource-demo/resource-demo.component';
+import { LinkedSignalDemoComponent } from './linked-signal/linked-signal-demo.component';
+import { isUserAuthenticated } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
+    canActivate: [isUserAuthenticated],
   },
   {
-    path: "login",
-    component: LoginComponent
+    path: 'login',
+    component: LoginComponent,
   },
   {
-    path: "lessons",
-    component: LessonsComponent
+    path: 'lessons',
+    component: LessonsComponent,
   },
   {
-    path:"shopping-cart",
-    component: LinkedSignalDemoComponent
+    path: 'shopping-cart',
+    component: LinkedSignalDemoComponent,
   },
   {
-    path: "resource-demo",
-    component: ResourceDemoComponent
+    path: 'resource-demo',
+    component: ResourceDemoComponent,
   },
   {
     path: '**',
-    redirectTo: '/'
-  }
+    redirectTo: '/',
+  },
 ];
