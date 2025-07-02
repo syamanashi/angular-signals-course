859fbd5a0e86b2fb9e3010de1a3d5304944100b7
syamanashi <sutton@suttonsoft.com>
2025-07-01 22:14:19 -0400

    New Section - Building the View Course Page & Implementing the Template of The Course Component

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Course } from '../models/course.model';
+import { Lesson } from '../models/lesson.model';
 
 @Component({
   selector: 'course',
   standalone: true,
   imports: [],
   templateUrl: './course.component.html',
-  styleUrl: './course.component.scss'
+  styleUrl: './course.component.scss',
 })
 export class CourseComponent {
+  course = signal<Course | null>(null);
 
+  lessons = signal<Lesson[]>([]);
 }
