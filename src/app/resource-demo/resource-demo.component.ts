19dbbbd62aafa090b08df9d6ce7099f739daacf9
syamanashi <sutton@suttonsoft.com>
2025-07-03 13:41:26 -0400

    Angular resource API - Everything that you need to know

--- src/app/resource-demo/resource-demo.component.ts
+++ src/app/resource-demo/resource-demo.component.ts
@@ -1,39 +1,54 @@
-import {Component, effect, inject, resource, signal} from "@angular/core";
-import {MatProgressSpinner} from "@angular/material/progress-spinner";
-import {environment} from "../../environments/environment";
-import {Lesson} from "../models/lesson.model";
-
+import {
+  Component,
+  effect,
+  inject,
+  resource,
+  ResourceRef,
+  signal,
+} from '@angular/core';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { environment } from '../../environments/environment';
+import { Lesson } from '../models/lesson.model';
 
 @Component({
   selector: 'resource-demo',
   templateUrl: './resource-demo.component.html',
   styleUrls: ['./resource-demo.component.scss'],
-  imports: [MatProgressSpinner]
+  imports: [MatProgressSpinner],
 })
 export class ResourceDemoComponent {
-
   env = environment;
 
   search = signal<string>('');
 
-  lessons = signal<Lesson[]>([])
+  lessons: ResourceRef<Lesson[]> = resource<Lesson[], { searchValue: string }>({
+    request: () => ({
+      searchValue: this.search(),
+    }),
+    loader: async ({ request, abortSignal }) => {
+      // destructures request value(s) passed in.
+      const response = await fetch(
+        `${this.env.apiRoot}/search-lessons?query=${request.searchValue}&courseId=18`,
+        {
+          signal: abortSignal, // not an angular signal, just the 'signal' api of the fetch method for aborting previous requests.
+        }
+      );
+      const json = await response.json();
+      return json.lessons; // lessons property in the response json.
+    },
+  });
 
   constructor() {
-
     effect(() => {
-      console.log('searching lessons:', this.search() );
-    })
+      console.log('searching lessons:', this.search());
+    });
   }
 
   searchLessons(search: string) {
     this.search.set(search);
   }
 
-  reset() {
+  reset() {}
 
-  }
-
-  reload() {
-
-  }
+  reload() {}
 }
